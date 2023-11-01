41f2489bc02f890ede16658a4288a1928e5eca6d
johana2020 <johanamejia33@gmail.com>
2023-11-01 12:52:35 -0500

    introduccion js

---
+++ js/introduccion.js
@@ -0,0 +1,49 @@
+/*front 
+scripts EMCaScripts  isomorfico 
+express como mongo
+no necesita tipo de dato en las variables ni : ;  
+motor de ejecucion de java google b8
+fire fox spikermonkey
+para usar express hay que usar nodejs instalar
+let o const para variables 
+
+
+$name
+_name
+name*/
+// asi declaro variables
+
+let numero = 1;
+let miVariable = true; 
+let nombre= "johana";
+let miVariables= [1, "bob"] //arreglo
+//pi = 3,1416;
+//const pi;
+//console. log(´numero ${num1} es muy lindo¨ /n ´)//incrustar
+
+let a=5;
+let b=10;
+console.log(`la suma es ${a+b}`);
+
+/*  operadores 
+aritmeticos + - * / mod %
+de comparacion == || === mira hasta el tipo de dato !=  !== <> >= <=
+logico || && ! 
+console.info()
+console.warn()
+console.error()
+console.clear()vctrl+l
+
+*/
+
+let num1 =parseInt(prompt("digite el primer numero"))
+let num2 =parseInt(prompt("digite el segundo numero"))
+console.log(`la suma es ${num1+num2}`);
+// modificar el tipo de dato o parsear 
+
+
+
+
+
+
+
